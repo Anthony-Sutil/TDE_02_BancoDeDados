0b4b8eb9bed1b21bf534bc079f08894a2c325a2e
Anthony-Sutil <thonysutil@gmail.com>
2024-04-08 17:37:15 -0300

    continuacao substituicao de tabela

--- models.py
+++ models.py
@@ -15,22 +15,14 @@ class Autores(Base):
     data_nascimento: Mapped[date] = mapped_column(DATE, nullable=False)
     biografia: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
 
-class Avaliacao(Base):
-    __tablename__ = "avaliacao"
-    id_avaliacao: Mapped[int] = mapped_column(INTEGER, unique=True, autoincrement=True, nullable=False, primary_key=True)
-    estrelas: Mapped[int] = mapped_column(TINYINT, nullable=False, unsigned=True)
-    data: Mapped[date] = mapped_column(DATE, nullable=False)
-    avaliador: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
-    livro_id_livro: Mapped[int] = mapped_column(INTEGER,ForeignKey(livro.id_livro), nullable=False)
-    cliente_id_cliente: Mapped[int] = mapped_column(INTEGER,ForeignKey(cliente.id_cliente), nullable=False)
-    
-
-class Categoria(Base):
-    __tablename__ = "categoria"
-    id_categoria: Mapped[int] = mapped_column(INTEGER, primary_key=True, unsigned=True, autoincrement=True)
+class Livro(Base):
+    __tablename__ = "livro"
+    id_livro: Mapped[int] = mapped_column(INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
     nome: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
-    classificacao_indicativa: Mapped[int] = mapped_column(TINYINT, nullable=False, unsigned=True)
-
+    preco: Mapped[float] = mapped_column(FLOAT, unsigned=True, nullable=True)
+    quantidade_estoque: Mapped[int] = mapped_column(INTEGER, unsigned=True, nullable=False)
+    quantidade_vendidas: Mapped[int] = mapped_column(SMALLINT, unsigned=True, nullable=False)
+    data_publicacao: Mapped[date] = mapped_column(DATE, nullable=False)
 
 class Cliente(Base):
     __tablename__ = "cliente"
@@ -42,6 +34,22 @@ class Cliente(Base):
     email: Mapped[str] = mapped_column(VARCHAR(50), nullable=False, unique=True)
     quantidade_compras: Mapped[int] = mapped_column(SMALLINT, nullable=False, default=0)
 
+class Avaliacao(Base):
+    __tablename__ = "avaliacao"
+    id_avaliacao: Mapped[int] = mapped_column(INTEGER, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    estrelas: Mapped[int] = mapped_column(TINYINT, nullable=False, unsigned=True)
+    data: Mapped[date] = mapped_column(DATE, nullable=False)
+    avaliador: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    livro_id_livro: Mapped[int] = mapped_column(INTEGER,ForeignKey(Livro.id_livro), nullable=False)
+    cliente_id_cliente: Mapped[int] = mapped_column(INTEGER,ForeignKey(Cliente.id_cliente), nullable=False)
+    
+
+class Categoria(Base):
+    __tablename__ = "categoria"
+    id_categoria: Mapped[int] = mapped_column(INTEGER, primary_key=True, unsigned=True, autoincrement=True)
+    nome: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    classificacao_indicativa: Mapped[int] = mapped_column(TINYINT, nullable=False, unsigned=True)
+
 class Editora(Base):
     __tablename__ = "editora"
     id_editora: Mapped[int] = mapped_column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
@@ -49,14 +57,7 @@ class Editora(Base):
     nome: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
     exemplares_vendidos: Mapped[int] = mapped_column(SMALLINT, nullable=False, default=0)
 
-class Livro(Base):
-    __tablename__ = "livro"
-    id_livro: Mapped[int] = mapped_column(INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
-    nome: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
-    preco: Mapped[float] = mapped_column(FLOAT, unsigned=True, nullable=True)
-    quantidade_estoque: Mapped[int] = mapped_column(INTEGER, unsigned=True, nullable=False)
-    quantidade_vendidas: Mapped[int] = mapped_column(SMALLINT, unsigned=True, nullable=False)
-    data_publicacao: Mapped[date] = mapped_column(DATE, nullable=False)
+
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
