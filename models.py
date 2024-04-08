b151a5d6c59754a0233606435d88323d84f504a2
Anthony-Sutil <thonysutil@gmail.com>
2024-04-08 17:20:21 -0300

    substituicao de tabela

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import DATE, VARCHAR, CHAR, Enum, \
-                        INTEGER, SMALLINT, BOOLEAN, ForeignKey
+                        INTEGER, SMALLINT, TINYINT, BOOLEAN, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from datetime import date
 from connection import engine
@@ -8,23 +8,26 @@ class Base(DeclarativeBase):
     pass
 
 
-class Pessoa(Base):
-    __tablename__ = "pessoa"
-    codigo: Mapped[int] = mapped_column('codigo', INTEGER, autoincrement=True, primary_key=True)
-    email_institucional: Mapped[str] = mapped_column(VARCHAR(50), unique=True, nullable=False)
+class autores(Base):
+    __tablename__ = "autores"
+    id_autores: Mapped[int] = mapped_column('id_autores', INTEGER, autoincrement=True, nullable=False, primary_key=True)
+    nome: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    data_nascimento: Mapped[date] = mapped_column(DATE, nullable=False)
+    biografia: Mapped[str] = mapped_column(VARCHAR(255), nullable=True)
 
-class PessoaFisica(Base):
-    __tablename__ = "pessoa_fisica"
-    codigo: Mapped[int] = mapped_column(INTEGER, ForeignKey(Pessoa.codigo), primary_key=True)
-    nome: Mapped[str] = mapped_column(VARCHAR(100), nullable=False)
-    cpf: Mapped[str] = mapped_column(CHAR(11), nullable=False, unique=True)
-    data_nasc: Mapped[date] = mapped_column(DATE, nullable=False)
-    email_pessoal: Mapped[str] = mapped_column(VARCHAR(50), nullable=False, unique=True)
+class Avaliacao(Base):
+    __tablename__ = "avaliacao"
+    id_avaliacao: Mapped[int] = mapped_column(INTEGER, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    estrelas: Mapped[int] = mapped_column(TINYINT, nullable=False, unsigned=True)
+    data: Mapped[date] = mapped_column(DATE, nullable=False)
+    avaliador: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    livro_id_livro: Mapped[int] = mapped_column(INTEGER,ForeignKey(livro.id_livro), nullable=False)
+    cliente_id_cliente: Mapped[int] = mapped_column(INTEGER,ForeignKey(cliente.id_cliente), nullable=False)
     
 
 class Filial(Base):
     __tablename__ = "filial"
-    codigo: Mapped[int] = mapped_column(INTEGER, ForeignKey(Pessoa.codigo), primary_key=True)
+    codigo: Mapped[int] = mapped_column(INTEGER, ForeignKey(autores.id_autores), primary_key=True)
     tipo: Mapped[str] = mapped_column(Enum("Centro Universitário", "Universidade", "Faculdade"), nullable=False)
 
 
