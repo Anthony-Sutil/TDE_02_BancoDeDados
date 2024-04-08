c39bd8be234740427d7916c0073d7003f746e7fd
Anthony-Sutil <thonysutil@gmail.com>
2024-04-08 17:37:56 -0300

    continuacao substituicao de tabela

--- connection.py
+++ connection.py
@@ -8,7 +8,7 @@ USER = "root"
 PASSWD = quote("root")
 HOST = "localhost" # 127.0.0.1
 PORT = 3306
-DATABASE = "teste"
+DATABASE = "testesql"
 
 url = f'mysql+pymysql://{USER}:{PASSWD}@{HOST}:{PORT}/{DATABASE}'
 
